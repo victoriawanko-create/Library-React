be5babbbc12b3b955044cafd91257212c230023e
victoriawanko-create. <victoriawanko@gmail.com.>
2026-06-30 00:09:55 -0400

    landing page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 import Nav from './components/Nav.jsx'
+import Landing from './components/Landing.jsx'
 
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
     </div>
   );
 }
