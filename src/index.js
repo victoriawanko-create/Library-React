fbcbd8a33addc010b15361c055900f41642ce83e
victoriawanko-create. <victoriawanko@gmail.com.>
2026-06-30 16:06:59 -0400

    4:06pm

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBars, faShoppingCart, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faBars, faShoppingCart)
+library.add(faBars, faShoppingCart, faBolt, faBookOpen, faTags)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
