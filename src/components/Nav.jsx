a933a90f46bfb7303e74762afdaa37b56852da67
victoriawanko-create. <victoriawanko@gmail.com.>
2026-06-29 23:34:22 -0400

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import LibraryLogo from '../assets/Library.svg';
 
 const Nav = () => {
   return (
     <nav> 
          <div className="nav__container">
             <a href ="/">
-            <img src="" className="logo" />
+            <img src={LibraryLogo} className="logo" />
             </a>
             <ul className="nav__links">
                 <li className="nav__list">
@@ -19,9 +20,38 @@ const Nav = () => {
                     Books
                     </a>
                 </li>
-                <button className="btn__menu"></button>
+                <button className="btn__menu">
                 <FontAwesomeIcon icon="bars" />
+                </button>
+                <li className="nav__icon">
+                <a href="/cart" className="nav__link">
+                <FontAwesomeIcon icon="shopping-cart" />
+                </a>
+                <span className="cart__length">2</span>
+                </li>
             </ul>
+            <div className="menu__backdrop">
+              <button className="btn__menu btn__menu--close">
+                <FontAwesomeIcon icon="times" />
+                </button>
+                <ul className="menu__links">
+                  <li>
+                    <a href="/" className="menu__link">
+                    Home
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/" className="menu__link">
+                    Books
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/" className="menu__link">
+                    Cart
+                    </a>
+                  </li>
+                </ul>
+            </div>
          </div>
     </nav>
   );
